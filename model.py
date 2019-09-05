aff0839c2605849be26143ed42b643065bc280cd
Tarun Venkatesh H <43094053+tarunh9@users.noreply.github.com>
2019-09-05 17:58:31 +0530

    Update model.py

--- model.py
+++ model.py
@@ -46,10 +46,7 @@ def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
+
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
